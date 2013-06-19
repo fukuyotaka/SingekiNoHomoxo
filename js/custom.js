95682714b39ffe70d76380d05646186a5872b046
Lin <silverrod@live.jp>
2013-06-19 22:01:25 +0900

    ┌(┌^o^)┐

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,40 @@
-$(document).ready(function () {
+﻿$(document).ready(function () {
+    var posx = 300
+    var posy = 300
+
+    $("canvas").drawText({
+        fillStyle: "#9cf",
+        strokeWidth: 2,
+        x: posx, y: posy,
+        fontSize: "36pt",
+        text: "┌(┌^o^)┐"
+    });
 
     $(window).keydown(function (e) {
         $("#key").text(e.keyCode);
+        if (e.keyCode == 65 || e.keyCode == 68 || e.keyCode == 83 || e.keyCode == 87) {
+
+            $("canvas").clearCanvas()
+            if (e.keyCode == 65) {
+                posx = posx - 5
+            }
+            if (e.keyCode == 68) {
+                posx = posx + 5
+            }
+            if (e.keyCode == 83) {
+                posy = posy + 5
+            }
+            if (e.keyCode == 87) {
+                posy = posy - 5
+            }
+            $("canvas").drawText({
+                fillStyle: "#9cf",
+                strokeWidth: 2,
+                x: posx, y: posy,
+                fontSize: "36pt",
+                text: "┌(┌^o^)┐"
+            });
+        }
         return false;
     });
 
