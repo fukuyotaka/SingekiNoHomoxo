7d8ea00948965bab8dd772f5f8d13ae6f0624490
kondo <taku220@gmail.com>
2013-06-26 16:47:55 +0900

    重力を追加(仮)

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,29 @@
 ﻿var move = new Array(false, false, false, false);
 //move[]={left,right,down,up}
+var wire = new Array(false,false);
+//wire[]={left,right}
 var posx = 300
 var posy = 300
+
+var grabity = 3
 
 $(function () {
     refresh()
 });
 
+$(function flip(){
+    refresh();
+    if(posy<370){
+        posy = posy + grabity;
+    }
+    setTimeout(function(){
+        flip();
+    },30);
+});
+
 $(window).keydown(function (e) {
     $("#key").text(e.keyCode);
-    if (e.keyCode == 65 || e.keyCode == 68 || e.keyCode == 83 || e.keyCode == 87) {
+    if (e.keyCode == 65 || e.keyCode == 68 || e.keyCode == 83 || e.keyCode == 87 || e.KeyCode == 81 || e.KeyCode == 69) {
         if (e.keyCode == 65) {
             move[0] = true
         }
@@ -21,6 +35,13 @@ $(window).keydown(function (e) {
         }
         if (e.keyCode == 87) {
             move[3] = true
+        }
+
+        if (e.keyCode == 81) {
+            wire[0] = true
+        }
+        if (e.keyCode == 69) {
+            wire[0] = true
         }
         refresh()
     }
@@ -42,6 +63,12 @@ $(window).keyup(function (e) {
         if (e.keyCode == 87) {
             move[3] = false
         }
+        if(e.KeyCode == 81) {
+            wire[0] = false
+        }
+        if(e.KeyCode ==69) {
+            wire[1] = false
+        }
         refresh()
     }
     return false;
@@ -60,12 +87,38 @@ function refresh() {
     if (move[3] == true) {
         posy = posy - 10
     }
+    if (wire[0] == true) {
+
+    }else if (wire[1] == true) {
+
+    }
+
     $("canvas").clearCanvas()
     $("canvas").drawText({
         fillStyle: "#9cf",
         strokeWidth: 2,
         x: posx, y: posy,
-        fontSize: "36pt",
+        fontSize: "10pt",
         text: "┌(┌^o^)┐"
     });
+
+    $("canvas").drawLine({
+     strokeStyle: "#000",
+     strokeWidth: 1,
+     strokeCap: "round",
+     strokeJoin: "round",
+
+     x1: 50, y1: 50,
+     x2: posx-10, y2: posy-10
+ });
+
+    $("canvas").drawRect({
+        fillStyle: "#fc9",
+        x:0,
+        y:400,
+        width: 1600,
+        height: 50
+    });
+
+
 }
